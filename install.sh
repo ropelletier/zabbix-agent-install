57ef783bfc178a248c7d2d0dcdcd01c50ee8c326
ropelletier <88781432+ropelletier@users.noreply.github.com>
2026-02-03 10:19:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 wget https://repo.zabbix.com/zabbix/7.2/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.2+debian12_all.deb
 sudo dpkg -i zabbix-release_latest_7.2+debian12_all.deb
 sudo apt update
-sudo apt install zabbix-agent
+sudo apt install zabbix-agent -y
 sudo nano /etc/zabbix/zabbix_agentd.conf
